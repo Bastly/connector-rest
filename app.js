3d3e99135cea57ebcae6a8cd05fbe2168d591040
elchudi <gustavogiudici@gmail.com>
2015-03-12 15:49:27 -0300

    initial implementation of connector rest

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 
+var IP_ATAHUALPA = '127.0.0.1';
+var bastly = require('bastly')({ipAtahualpa:IP_ATAHUALPA});
+
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
+console.log('Listenint to port: ', port);
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -14,12 +18,18 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/requestChaski', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    bastly.getWorker('channelId', function(reply){
+        console.log('got reply: ' + reply);
+        res.json({ message: reply  });   
+    });
 });
 
 router.post('/publishMessage', function(req, res) {
-    res.json({ message: 'ok' });   
-    //publish message to atahualpa
+    
+    bastly.sendMessage(req.body.channel, req.body.message, function(repply){
+        console.log('messasge ack!'); 
+        res.json({ message: 'ok' });   
+    });
 });
 
 // REGISTER OUR ROUTES -------------------------------
