8872d1cbaa0e9e5744cff2cca35abe3cadf720eb
Alex Vidal <goofyahead@gmail.com>
2015-06-01 17:47:57 +0200

    request chasky paramas

--- connector-rest.js
+++ connector-rest.js
@@ -50,12 +50,13 @@ var router = express.Router();              // get an instance of the express Ro
 
 // chaski worker request
 router.get('/requestChaski', function(req, res) {
+    console.log(req.params);
     var channel = req.params.channel;
     var from = req.params.from;
     var apiKey = req.params.apiKey;
    
     if(!channel || !from || !apiKey){
-        res.json({ message: "must specify channel, from and apiKey"  });
+        res.status(404).body({ message: "must specify channel, from and apiKey"  });
         return;
     }
 
